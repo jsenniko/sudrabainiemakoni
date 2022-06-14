046606c115aacab85f446c310db130c177e0abef
jsenniko <86888784+jsenniko@users.noreply.github.com>
2022-06-14 21:16:45 +0300

    Setup.py and requirements added

---
+++ setup.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='sudrabainiemakoni',
+	version='0.01',
+	description='Sudrabaino mākoņu sinhronās novērošanas datu apstrādes rīki',
+	author='Juris Seņņikovs',
+	author_email='jsenniko@latnet.lv',
+	install_requires=['numpy',
+		'pandas',
+		'scipy',
+		'matplotlib',
+		'scikit-image',
+		'astropy','exifread','cameratransform','pymap3d','tilemapbase'],
+	packages=['sudrabainiemakoni'],
+	zip_safe=False)
+
+__version__ = "0.1"
